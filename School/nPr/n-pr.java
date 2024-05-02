a82abce1ea5c5986fe2ebceafb1b941adddaff19
Arafat <94069423+aliArafat9@users.noreply.github.com>
2024-05-02 21:07:06 +0530

    Added Solution - GfG to GitHub

---
+++ School/nPr/n-pr.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            Long n = Long.parseLong(a[0]);
+            Long r = Long.parseLong(a[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.nPr(n, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long nPr(long n, long r){
+        // code here
+        long f=1;
+        for(int i=1;i<=r;i++){
+           f *= (n - i + 1);
+        }
+        return f;
+    }
+}
