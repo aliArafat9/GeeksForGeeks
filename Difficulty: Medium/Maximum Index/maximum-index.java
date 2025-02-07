600c36a19b4961563557b7e886b784f292c19b75
Arafat <94069423+aliArafat9@users.noreply.github.com>
2025-02-07 23:33:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Index/maximum-index.java
+++ Difficulty: Medium/Maximum Index/maximum-index.java
@@ -1,60 +1,46 @@
 //{ Driver Code Starts
-// Initial Template for Java
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = Integer.parseInt(scanner.nextLine().trim());
-
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
         while (t-- > 0) {
-            String line = scanner.nextLine().trim();
-            String[] numsStr = line.split(" ");
-            int[] nums = new int[numsStr.length];
-            for (int i = 0; i < numsStr.length; i++) {
-                nums[i] = Integer.parseInt(numsStr[i]);
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
 
-            Solution ob = new Solution();
-            System.out.println(ob.maxIndexDiff(nums));
-        
-System.out.println("~");
-}
+            System.out.println(new Solution().maxIndexDiff(arr));
+        }
     }
 }
-
 // } Driver Code Ends
 
 
-// User function Template for Java
 class Solution {
-    // Function to find the maximum index difference.
-    int maxIndexDiff(int[] arr) {
+    int maxIndexDiff(int arr[]) {
         int n=arr.length;
-        int res=0;
-        
-        // monotonic decreasing stack
-        
-        Stack<Integer> st=new Stack<>();
-        
-        for(int i=0;i<n;i++){
-            if(st.isEmpty() || arr[st.peek()]>=arr[i]){
-                st.push(i);
+        if(n==0) return 0;
+        int leftMin[]=new int[n];
+        leftMin[0]=arr[0];
+        for(int i=1;i<n;i++) leftMin[i]=Math.min(leftMin[i-1],arr[i]);
+        int rightMax[]=new int[n];
+        rightMax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--) rightMax[i]=Math.max(rightMax[i+1],arr[i]);
+        int i=0;
+        int j=0;
+        int idx=-1;
+        while(i<n && j<n){
+            if(leftMin[i]<=rightMax[j]){
+                idx=Math.max(idx,j-i);
+                j++;
             }
+            else i++;
         }
-        
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && arr[i]>=arr[st.peek()]){
-                int ind=st.pop();
-                res=Math.max(res,i-ind);
-            }
-        }
-        
-        return res;
-        
-        
-        
-        
+        return idx;
     }
 }
